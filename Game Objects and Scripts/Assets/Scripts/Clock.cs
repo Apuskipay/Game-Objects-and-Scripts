ab55ce86665eab9e05bf08f8ece08521f6ad7c28
Daniel Rondon <danielrondon00@gmail.com>
2021-07-28 17:36:00 -0400

    Clock Script Finished, Projects Complete.
    The script that makes the clock give the time has been finished and the project is complete.

---
+++ Game Objects and Scripts/Assets/Scripts/Clock.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour 
+{
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    void Update ()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+    }
+}
